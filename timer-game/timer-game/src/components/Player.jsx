3ebd213c07b3007d52b4b9dc08794e98779da2ac
nmohammednawaz-egov <mohammed.nawaz@egovernments.org>
2024-03-21 14:52:37 +0530

    Timer Game

---
+++ timer-game/timer-game/src/components/Player.jsx
@@ -0,0 +1,23 @@
+import { useState, useRef } from "react";
+
+export default function Player() {
+
+  const playerName = useRef();
+
+  const [enteredPlayerName, setEnteredPlayerName] = useState();
+
+  function handleClick(){
+    setEnteredPlayerName(playerName.current.value);
+    playerName.current.value = '';
+  }
+
+  return (
+    <section id="player">
+      <h2>Welcome {enteredPlayerName ?? 'Player Name'}</h2>
+      <p>
+        <input type="text" ref={playerName}/>
+        <button onClick={handleClick}>Set Name</button>
+      </p>
+    </section>
+  );
+}
